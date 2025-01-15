b805df008be6bd9131887672b84dcf546b057903
manfrim <rafaelmanfrim2004@gmail.com>
2025-01-15 20:39:42 -0300

    Hash para senha de usuário e validação de e-mail

--- src/http/controllers/register.ts
+++ src/http/controllers/register.ts
@@ -1,4 +1,5 @@
 import { prisma } from '@/lib/prisma'
+import { hash } from 'bcryptjs'
 import { FastifyReply, FastifyRequest } from 'fastify'
 import { z } from 'zod'
 
@@ -11,8 +12,18 @@ export async function register(request: FastifyRequest, reply: FastifyReply) {
 
   const { name, email, password } = registerBodySchema.parse(request.body)
 
+  const userWithSameEmail = await prisma.user.findUnique({
+    where: { email },
+  })
+
+  if (userWithSameEmail) {
+    return reply.status(409).send()
+  }
+
+  const passwordHash = await hash(password, 6)
+
   await prisma.user.create({
-    data: { name, email, password_hash: password },
+    data: { name, email, password_hash: passwordHash },
   })
 
   return reply.status(201).send()
