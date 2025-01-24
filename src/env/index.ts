e7c3947a295a32cd72ec0535bc7c40b477e12347
manfrim <rafaelmanfrim2004@gmail.com>
2025-01-24 20:37:37 -0300

    Autenticação com JWT

--- src/env/index.ts
+++ src/env/index.ts
@@ -4,6 +4,7 @@ import { z } from 'zod'
 const envSchema = z.object({
   NODE_ENV: z.enum(['dev', 'test', 'production']).default('dev'),
   PORT: z.coerce.number().default(3333),
+  JWT_SECRET: z.string(),
 })
 
 const { success, data, error } = envSchema.safeParse(process.env)
--- src/http/routes.ts
+++ src/http/routes.ts
@@ -2,8 +2,12 @@ import { FastifyInstance } from 'fastify'
 
 import { register } from './controllers/register'
 import { authenticate } from './controllers/authenticate'
+import { profile } from './controllers/profile'
 
 export function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  // These routes is protected by the JWT
+  app.get('/me', profile)
 }
