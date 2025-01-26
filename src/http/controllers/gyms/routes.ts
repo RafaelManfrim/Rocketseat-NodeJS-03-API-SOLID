5488238a57082f149f92802b7b575d82da5b1704
manfrim <rafaelmanfrim2004@gmail.com>
2025-01-26 13:05:06 -0300

    Controllers de Academia

--- src/http/controllers/gyms/routes.ts
+++ src/http/controllers/gyms/routes.ts
@@ -1,7 +1,15 @@
 import { FastifyInstance } from 'fastify'
 
 import { verifyJWT } from '../../middlewares/verify-jwt'
+import { nearby } from './nearby'
+import { searh } from './search'
+import { create } from './create'
 
 export function gymsRoutes(app: FastifyInstance) {
   app.addHook('onRequest', verifyJWT)
+
+  app.get('/gyms/search', searh)
+  app.get('/gyms/nearby', nearby)
+
+  app.post('/gyms', create)
 }
---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,24 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+
+import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms-use-case'
+
+export async function searh(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymQuerySchema = z.object({
+    search: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { search, page } = searchGymQuerySchema.parse(request.body)
+
+  const searchGymsUseCase = makeSearchGymsUseCase()
+
+  const { gyms } = await searchGymsUseCase.execute({
+    page,
+    query: search,
+  })
+
+  return reply.send({
+    gyms,
+  })
+}
